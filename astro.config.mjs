73d299757db54fdcef30d45674673d42dd2db79c
MiniWater <1456441157@qq.com>
2026-04-30 00:28:52 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://blog.krjojo.com',
+    base: '/',
+    // trailingSlash: "always",        // always强制所有 URL 末尾带斜杠 never不带斜杠
+});
