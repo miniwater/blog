a462d956bc9addc462c398afd7d9571771e424a3
kuangrongjian <krjojo@qq.com>
2026-04-30 14:09:31 +0800

    add React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://blog.krjojo.com',
-    base: '/',
-    // trailingSlash: "always",        // always强制所有 URL 末尾带斜杠 never不带斜杠
+  site: 'https://blog.krjojo.com',
+
+  // trailingSlash: "always",        // always强制所有 URL 末尾带斜杠 never不带斜杠
+  base: '/',
+
+  integrations: [react()]
 });
