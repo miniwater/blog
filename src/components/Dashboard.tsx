92683e3d4e5dcdf80fd7a380e31b9ff7c43e362d
MiniWater <1456441157@qq.com>
2026-05-06 23:42:54 +0800

    switch 修复rtl

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -13,40 +13,43 @@ import {
     SidebarTrigger,
 } from "@/components/ui/sidebar"
 import { TooltipProvider } from "./ui/tooltip"
+import { DirectionProvider } from "@/components/ui/direction"
 
 export function Dashboard({ children, favoritePosts, url }: { children: React.ReactNode; favoritePosts: any[]; url: URL }) {
     return (
-        <TooltipProvider delayDuration={0}> {/* 包裹在最外层 */}
-            <SidebarProvider>
-                <SidebarLeft favoritePosts={favoritePosts} url={url} />
-                <SidebarInset>
-                    <header className="sticky top-0 flex h-14 shrink-0 items-center gap-2 bg-background">
-                        <div className="flex flex-1 items-center gap-2 px-3">
-                            <SidebarTrigger />
-                            <Separator
-                                orientation="vertical"
-                                className="mr-2 data-[orientation=vertical]:h-4"
-                            />
-                            <Breadcrumb>
-                                <BreadcrumbList>
-                                    <BreadcrumbItem>
-                                        <BreadcrumbPage className="line-clamp-1">
-                                            Project Management & Task Tracking
-                                        </BreadcrumbPage>
-                                    </BreadcrumbItem>
-                                </BreadcrumbList>
-                            </Breadcrumb>
+        // <DirectionProvider dir="rtl">
+            <TooltipProvider delayDuration={0}> {/* 包裹在最外层 */}
+                <SidebarProvider>
+                    <SidebarLeft favoritePosts={favoritePosts} url={url} />
+                    <SidebarInset>
+                        <header className="sticky top-0 flex h-14 shrink-0 items-center gap-2 bg-background">
+                            <div className="flex flex-1 items-center gap-2 px-3">
+                                <SidebarTrigger />
+                                <Separator
+                                    orientation="vertical"
+                                    className="mr-2 data-[orientation=vertical]:h-4"
+                                />
+                                <Breadcrumb>
+                                    <BreadcrumbList>
+                                        <BreadcrumbItem>
+                                            <BreadcrumbPage className="line-clamp-1">
+                                                Project Management & Task Tracking
+                                            </BreadcrumbPage>
+                                        </BreadcrumbItem>
+                                    </BreadcrumbList>
+                                </Breadcrumb>
+                            </div>
+                        </header>
+                        <div className="flex flex-1 flex-col gap-4 p-4">
+                            {/* <div className="mx-auto h-24 w-full max-w-3xl rounded-xl bg-muted/50" /> */}
+                            <div className="mx-auto w-full max-w-3xl rounded-xl" >
+                                {children}
+                            </div>
                         </div>
-                    </header>
-                    <div className="flex flex-1 flex-col gap-4 p-4">
-                        {/* <div className="mx-auto h-24 w-full max-w-3xl rounded-xl bg-muted/50" /> */}
-                        <div className="mx-auto w-full max-w-3xl rounded-xl" >
-                            {children}
-                        </div>
-                    </div>
-                </SidebarInset>
-                <SidebarRight />
-            </SidebarProvider>
-        </TooltipProvider>
+                    </SidebarInset>
+                    <SidebarRight />
+                </SidebarProvider>
+            </TooltipProvider>
+        // </DirectionProvider>
     )
 }
