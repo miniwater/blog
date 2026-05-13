6a7fb1ed050e063637b7da824fe471dd55adb24c
kuangrongjian <krjojo@qq.com>
2026-05-13 11:22:02 +0800

    link

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -15,27 +15,29 @@ import { SquareArrowOutUpRightIcon } from "lucide-react"
 export function FriendLinks() {
     return (
         <ItemGroup className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {friends.map((friend, index) => (
-                <Item key={friend.name} variant="outline">
-                    <ItemMedia>
-                        <Avatar>
-                            <AvatarImage src={friend.avatar.src} />
-                            <AvatarFallback>{friend.name.charAt(0)}</AvatarFallback>
-                        </Avatar>
-                    </ItemMedia>
-                    <ItemContent className="gap-1">
-                        <ItemTitle>{friend.name}</ItemTitle>
-                        <ItemDescription>{friend.description}</ItemDescription>
-                    </ItemContent>
-                    <ItemActions>
-                        <Button variant="ghost" size="icon" className="rounded-full">
-                            <a href={friend.url} target="_blank" rel="noopener noreferrer">
-                                <SquareArrowOutUpRightIcon />
-                            </a>
-                        </Button>
-                    </ItemActions>
-                </Item>
-            ))}
+            {friends
+                .sort((a, b) => a.name.localeCompare(b.name, 'zh-CN'))
+                .map((friend, index) => (
+                    <Item key={friend.name} variant="outline">
+                        <ItemMedia>
+                            <Avatar>
+                                <AvatarImage src={friend.avatar.src} />
+                                <AvatarFallback>{friend.name.charAt(0)}</AvatarFallback>
+                            </Avatar>
+                        </ItemMedia>
+                        <ItemContent className="gap-1">
+                            <ItemTitle>{friend.name}</ItemTitle>
+                            <ItemDescription>{friend.description}</ItemDescription>
+                        </ItemContent>
+                        <ItemActions>
+                            <Button variant="ghost" size="icon" className="rounded-full">
+                                <a href={friend.url} target="_blank" rel="noopener noreferrer">
+                                    <SquareArrowOutUpRightIcon />
+                                </a>
+                            </Button>
+                        </ItemActions>
+                    </Item>
+                ))}
         </ItemGroup>
     )
 }
